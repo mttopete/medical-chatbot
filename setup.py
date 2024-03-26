b62c2fad75f71fadd8c8d02eb9d3760194c47230
mttopete <mateotopete@gmail.com>
2024-03-25 20:19:22 -0700

    initialized application files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version='0.0.1',
+    author = 'mateo topete',
+    author_email='mateotopete@gmail.com',
+    packages = find_packages(),
+    install_requires=[]
+)
